71b58a90bd1e6007fd65a846e3812085a7573470
mathru <user@mathru.net>
2020-10-14 23:54:27 +0900

    saveのインターフェースの変更

--- lib/core/documentmodel.dart
+++ lib/core/documentmodel.dart
@@ -56,10 +56,11 @@ abstract class DocumentModel<TDocument extends IDataDocument>
   /// The [data] is the value to be saved in the document. If the data exists in the current document, it will be overwritten.
   ///
   /// The [builder] can handle the document as it is after the data is saved.
-  Future<T> save<T extends IDataDocument>() {
+  Future<T> save<T extends IDataDocument>(
+      {Map<String, dynamic> data, void builder(T document)}) {
     TDocument state = this.state;
     if (state == null) return Future.delayed(Duration.zero);
-    return state.save<T>();
+    return state.save<T>(data: data, builder: builder);
   }
 
   /// Deletes the document data.
